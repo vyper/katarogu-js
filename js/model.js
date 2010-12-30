e98805e56df84a775d0bd6e1e5f2140d65ca03ad
Gonzalo Gorosito <xgonzalo@gmail.com>
2010-12-30 17:38:10 -0300

    Carrinho usando novo data model e codigo migrado do html a um arquivo JS

--- js/model.js
+++ js/model.js
@@ -1,70 +1,110 @@
- /**
+/**
  * Katarogu model application class
- * @author vyper, ggorosito 
- * @package Katarogy
+ * @author ggorosito
+ * @package Katarogu
  * @date 12/30/2010
  */
- 
 function Model() {
-    
     this.items = new Array();
-    
-    this.errors = new Array();
-    
-    this.init();
 }
 
-Model.prototype.init() = function() {
-    
-    
+Model.prototype.init = function( items ) {
+    for( var i = 0; i < items.length; i++ ) {
+	this.add( items[i] );
+    }
 }
 
-Model.prototype.append = function( item ){
+Model.prototype.add = function( item ) {
     if( typeof item == 'object' ) {
-	this.items.push( item );
+	
+	if( !this.items[item.id] ) {
+	    this.items[item.id] = item;
+	}
+	else {
+	    this.items[item.id].count = this.items[item.id].count + item.count;
+	}
     }
     else {
-	throw "Not a product object dude!";
+	throw "Cannot add a non-object dude!";
     }
 }
 
 Model.prototype.delete = function( id ) {
-    var item = this.findById( id );
+    var item = this.items[id];
     
-    if( item.index ) {
-	delete this.items[item.index];
+    item.count = item.count - 1;
+    if( item.count <= 0 ) {
+	delete this.items[id];
     }
 }
 
-Model.prototype.findById = function( id ) {
-    var result = null;
-    
-    for( var i = 0; i < this.items.lenght; i++ ) {
-	var item = this.items[i];
-	
-	if( item.id == id ) {
-	    result = { index: i, item: item };
-	    
-	    break;
+Model.prototype.findByName = function( name ) {
+    for( item in this.items ) {
+	if( item.name.match( name ) ) {
+	    return item;
 	}
+    }
+    
+    return null;
+}
+
+Model.prototype.getAll = function() {
+    return this.items;
+}
+
+/**
+ * Stock model class
+ * @author ggorosito
+ * @package Katarogu
+ * @date 12/30/2010
+ */
+Stock.prototype = new Model();
+Stock.prototype.parent = Model.prototype;
+function Stock( items ) { 
+    this.init( items );
+}
+
+
+/**
+ * User cart model class
+ * @author ggorosito
+ * @package Katarogu
+ * @date 12/30/2010
+ */
+Cart.prototype = new Model();
+Cart.prototype.constructor = Cart;
+Cart.prototype.parent = Model.prototype;
+function Cart() { 
+    this.total = 0;
+    this.parent.items = this.items;
+}
+
+
+Cart.prototype.add = function( item ) {
+    var item_b = $.extend( {}, item );
+    
+    item_b.count = 1;
+    this.parent.add( item_b );
+}
+
+Cart.prototype.countItems = function() {
+    var items = this.getAll();
+    var result = 0;
+    
+    for( id in items ) {
+	result = result + items[id].count;
     }
     
     return result;
 }
 
-
-Stock.prototype = new Model();
-Stock.prototype.constructor = Stock;
-Stock.prototype.parent = Model.prototype;
-function Stock() { }
-
-Cart.prototype = new Model();
-Cart.prototype.constructor = Cart;
-Cart.prototype.parent = Model.prototype;
-function Cart() { }
-
-function Product() {
+Cart.prototype.getTotal = function() {
+    this.total = 0;
     
+    for( item in this.items ) {
+	this.total = this.total + ( this.items[item].price * this.items[item].count );
+    }
+    
+    return this.total;
 }
-
 
