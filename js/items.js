5dc9e8bff6a7a9bc7d57856dbea3fbf57297291e
Leonardo Saraiva <vyper@maneh.org>
2011-01-06 14:38:57 -0200

    Inclusão do arquivo de items.

---
+++ js/items.js
@@ -0,0 +1,6 @@
+var items = [
+    { id: 'acucareiro', name: 'Açucareiro', price: 23.00, count: 2 },
+    { id: 'assadeira-aluminio-redonda', name: 'Assadeira alumínio (redonda)', price: 9.90, count: 3 },
+    { id: 'assadeira-aluminio-retangular', name: 'Assadeira alumínio (retangular)', price: 19.90, count: 3 }
+];
+
