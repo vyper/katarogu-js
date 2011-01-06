c45a42191a465a34edafbe995bc62c3119f0e17a
Leonardo Saraiva <vyper@maneh.org>
2011-01-06 14:27:26 -0200

    Separação dos arquivos JavaScript do sistema e de configuração; Correção no README; Separação do arquivo de itens;

--- js/lib/application.js
+++ js/lib/application.js
@@ -5,12 +5,6 @@
  * @date 01/03/2011
  */
 
-var items = [
-    { id: 'acucareiro', name: 'Açucareiro', price: 23.00, count: 2 },
-    { id: 'assadeira-aluminio-redonda', name: 'Assadeira alumínio (redonda)', price: 9.90, count: 3 },
-    { id: 'assadeira-aluminio-retangular', name: 'Assadeira alumínio (retangular)', price: 19.90, count: 3 }
-];
-
 /**
  * Katarogu Object, this is the object that renders the entire application.
  * 
