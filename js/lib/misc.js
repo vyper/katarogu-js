17c5270bd3eadd33d149e923668858bfe2de3218
Leonardo Saraiva <leonardo@leonardo-saraivas-ibook-g4.local>
2011-01-15 00:57:27 -0200

    :% s/\t/     /

--- js/lib/misc.js
+++ js/lib/misc.js
@@ -32,13 +32,13 @@ String.prototype.skipAccents = function() {
 function float2moeda(num) {
     x = 0;
     if (num<0) {
-	    num = Math.abs(num);
-	    x = 1;
+        num = Math.abs(num);
+        x = 1;
     }
     
     if (isNaN(num)) {
-	    num = "0";
-	}
+        num = "0";
+    }
     
     cents = Math.floor((num*100+0.5) % 100);
     num = Math.floor((num*100+0.5) / 100).toString();
@@ -48,14 +48,14 @@ function float2moeda(num) {
     }
     
     for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++) {
-    	num = num.substring(0,num.length-(4*i+3)) + '.' +num.substring(num.length-(4*i+3));
+        num = num.substring(0,num.length-(4*i+3)) + '.' +num.substring(num.length-(4*i+3));
     }
     
     ret = num + ',' + cents;
     
     if (x == 1) {
-    	ret = ' - ' + ret;
+        ret = ' - ' + ret;
     }
     
-	return ret;
+    return ret;
 }
